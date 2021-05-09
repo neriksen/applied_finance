4330b261a2da94788122e401baaf083aa48bac2b
neriksen <nielseriksen@me.com>
2021-05-09 15:41:36 +0200

    Removed extreme values from sharpe calculation

--- sharpe.py
+++ sharpe.py
@@ -12,15 +12,19 @@ def run_checks(df):
 def calculate_sharpe(df):
     run_checks(df)
     
-    df['total_return_dual_phase'] = df.groupby(level=0, as_index=False).apply(lambda x: x.dual_phase/x.savings.cumsum()).reset_index(level=0, drop=True)
-    df['total_return_single_phase'] = df.groupby(level=0, as_index=False).apply(lambda x: x.single_phase/x.savings.cumsum()).reset_index(level=0, drop=True)
-    df['total_return_100'] = df.groupby(level=0, as_index=False).apply(lambda x: x['100']/x.savings.cumsum()).reset_index(level=0, drop=True)
-    df['total_return_9050'] = df.groupby(level=0, as_index=False).apply(lambda x: x['9050']/x.savings.cumsum()).reset_index(level=0, drop=True)
+    df['total_return_dual_phase'] = df.groupby(level=0, as_index=False).apply(lambda x: x.dual_phase/x.savings.cumsum()).reset_index(level=0, drop=True).pct_change().fillna(0)
+    df['total_return_single_phase'] = df.groupby(level=0, as_index=False).apply(lambda x: x.single_phase/x.savings.cumsum()).reset_index(level=0, drop=True).pct_change().fillna(0)
+    df['total_return_100'] = df.groupby(level=0, as_index=False).apply(lambda x: x['100']/x.savings.cumsum()).reset_index(level=0, drop=True).pct_change().fillna(0)
+    df['total_return_9050'] = df.groupby(level=0, as_index=False).apply(lambda x: x['9050']/x.savings.cumsum()).reset_index(level=0, drop=True).pct_change().fillna(0)
     
-    sharpe_dual = df.total_return_dual_phase.groupby(level=0).pct_change().groupby(level=0).mean()/df.total_return_dual_phase.groupby(level=0).pct_change().groupby(level=0).std()*sqrt(12)
-    sharpe_single = df.total_return_single_phase.groupby(level=0).pct_change().groupby(level=0).mean()/df.total_return_single_phase.groupby(level=0).pct_change().groupby(level=0).std()*sqrt(12)
-    sharpe_100 = df.total_return_100.groupby(level=0).pct_change().groupby(level=0).mean()/df.total_return_100.groupby(level=0).pct_change().groupby(level=0).std()*sqrt(12)
-    sharpe_9050 = df.total_return_9050.groupby(level=0).pct_change().groupby(level=0).mean()/df.total_return_9050.groupby(level=0).pct_change().groupby(level=0).std()*sqrt(12)
+    # Removes extreme pct change values
+    df = df[df.total_return_dual_phase.abs() < 1] 
+
+    sharpe_dual =   df.total_return_dual_phase.groupby(level=0).mean()/  df.total_return_dual_phase.groupby(level=0).std()*sqrt(12)
+    sharpe_single = df.total_return_single_phase.groupby(level=0).mean()/df.total_return_single_phase.groupby(level=0).std()*sqrt(12)
+    sharpe_100 =    df.total_return_100.groupby(level=0).mean()/         df.total_return_100.groupby(level=0).std()*sqrt(12)
+    sharpe_9050 =   df.total_return_9050.groupby(level=0).mean()/        df.total_return_9050.groupby(level=0).std()*sqrt(12)
+    
     
     sharpe_ratios = pd.concat([sharpe_dual, sharpe_single, sharpe_100, sharpe_9050], axis=1)
     sharpe_ratios.columns = ["Dual phase", "Single phase", "100% stocks", "Life cycle"]
